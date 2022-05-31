835a94bf521b721ae6cf6300f82812a6dbcf4713
Adriana Oviedo <oviedolopezadriana13@gmail.com>
2022-05-30 20:01:29 -0400

    qqq

--- consulta.js
+++ consulta.js
@@ -1,33 +1,27 @@
-const pool = require('/bd')
+const pool = require('./db')
 const insertar = async (datos) => {
-  // Paso 3
   const consulta = {
-    text: 'INSERT INTO ejercicios values($1, $2, $3, $4)',
+    text: 'INSERT INTO repertorio values($1, $2, $3)',
     values: datos,
   }
   try {
     const result = await pool.query(consulta)
     return result
   } catch (error) {
-    // Paso 4
     console.log(error.code)
     return error
   }
 }
-// Paso 1
 const consultar = async () => {
-  // Paso 2
   try {
     const result = await pool.query('SELECT * FROM ejercicios')
     return result
   } catch (error) {
-    // Paso 3
     console.log(error.code)
     return error
   }
 }
 const editar = async (datos) => {
-  // Paso 2
   const consulta = {
     text: `UPDATE ejercicios SET
   nombre = $1,
@@ -37,7 +31,6 @@ const editar = async (datos) => {
   WHERE nombre = $1 RETURNING *`,
     values: datos,
   }
-  // Paso 3
   try {
     const result = await pool.query(consulta)
     console.log(result)
@@ -47,9 +40,7 @@ const editar = async (datos) => {
     return error
   }
 }
-// Paso 1
 const eliminar = async (nombre) => {
-  // Paso 2
   try {
     const result = await pool.query(
       `DELETE FROM ejercicios WHERE nombre = '${nombre}'`
